a68ca86be4d4b82bbd43771c963f7877e41826b0
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2025-04-23 17:48:15 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- scripts/lib/larkDocWriter.js
+++ scripts/lib/larkDocWriter.js
@@ -1292,6 +1292,7 @@ class larkDocWriter {
         if (!this.page_blocks) {
             throw new Error('Page blocks not found');
         }
+
         // console.log(this.page_blocks)
         return this.page_blocks.find(x => x['block_id'] === block_id);
     }
@@ -1302,8 +1303,33 @@ class larkDocWriter {
 
         let prev = elements[elements.indexOf(element) - 1] || null;
         let prev_element_type = prev? prev['equation'] ? 'equation' : 'text_run' : null;
+        let next = elements[elements.indexOf(element) + 1] || null;
+        let next_element_type = next? next['equation'] ? 'equation' : 'text_run' : null;
+        let rip_off_line_breaks = false;
 
-        if (!(prev && prev[prev_element_type]['content'].endsWith('\n'))) content = content.trimEnd();
+        // single element 
+        if ((!prev || prev_element_type === 'text_run') && (!next || next_element_type === 'text_run')) {
+            content = content.trim();
+        }
+
+        // last element
+        if (prev && prev_element_type === 'equation' && (!next || next_element_type === 'text_run')) {
+            if (rip_off_line_breaks) content = content.trim()
+        }
+
+        // first element
+        if ((!prev || prev_element_type === 'text_run') && next && next_element_type === 'equation') {
+            content = content.trim();
+
+            if (!(prev && prev['text_run']['content'].endsWith('\n'))) {
+                rip_off_line_breaks = true;
+            }
+        }
+
+        // middle element
+        if (prev && prev_element_type === 'equation' && next && next_element_type === 'equation') {
+            content = content.trim();
+        }
 
         return content;        
     }
@@ -1355,8 +1381,9 @@ class larkDocWriter {
     }
 
     __style_markdown(element, elements, style_name, decorator) {
-        let content = element['text_run']['content'];
-        let style = element['text_run']['text_element_style'];
+        let element_type = element['equation'] ? 'equation' : 'text_run';
+        let content = element[element_type]['content'];
+        let style = element[element_type]['text_element_style'];
 
         let prev = elements[elements.indexOf(element) - 1] || null;
         let prev_element_type = prev? prev['equation'] ? 'equation' : 'text_run' : null;
@@ -1365,26 +1392,26 @@ class larkDocWriter {
 
         if (!content.match(/^\s+$/)) {
             // single element
-            if ((!prev || (prev && !prev[prev_element_type]['text_element_style'][style_name])) && style[style_name] && (!next || (next && !next[next_element_type]['text_element_style'][style_name]))) {
+            if ((!prev || prev_element_type === 'equation' || (prev && !prev[prev_element_type]['text_element_style'][style_name])) && style[style_name] && (!next || next_element_type === 'equation' || (next && !next[next_element_type]['text_element_style'][style_name]))) {
                 let prefix_spaces = content.match(/^\s*/)[0];
                 let suffix_spaces = content.match(/\s*$/)[0];
                 content = `${prefix_spaces}${decorator}${content.trim()}${decorator}${suffix_spaces}`;
             }
 
             // first element
-            if ((!prev || (prev && !prev[prev_element_type]['text_element_style'][style_name])) && style[style_name] && next && next[next_element_type]['text_element_style'][style_name]) {
+            if ((!prev || prev_element_type === 'equation' || (prev && !prev[prev_element_type]['text_element_style'][style_name])) && style[style_name] && next && next_element_type === 'text_run' && next[next_element_type]['text_element_style'][style_name]) {
                 let prefix_spaces = content.match(/^\s*/)[0];
                 content = `${prefix_spaces}${decorator}${content.trimStart()}`;
             }
 
             // last element
-            if (prev && prev[prev_element_type]['text_element_style'][style_name] && style[style_name] && (!next || (next && !next[next_element_type]['text_element_style'][style_name]))) {
+            if (prev && prev_element_type === 'text_run' && prev[prev_element_type]['text_element_style'][style_name] && style[style_name] && (!next || next_element_type === 'equation' || (next && !next[next_element_type]['text_element_style'][style_name]))) {
                 let suffix_spaces = content.match(/\s*$/)[0];
                 content = `${content.trimEnd()}${decorator}${suffix_spaces}`;
             }
 
             // middle element
-            if (prev && prev[prev_element_type]['text_element_style'][style_name] && style[style_name] && next && next[next_element_type]['text_element_style'][style_name]) {
+            if (prev && prev_element_type === 'text_run' && prev[prev_element_type]['text_element_style'][style_name] && style[style_name] && next && next_element_type === 'text_run' && next[next_element_type]['text_element_style'][style_name]) {
                 content = `${content}`;
             }
         }
